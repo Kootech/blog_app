0c198461654ff6db275dcd54730135e2ac2892db
kelvin <kitetesh.km@gmail.com>
2022-09-26 17:11:46 +0300

    dockerized app

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const uri = "http://127.0.0.1:8000/api/";
+const uri = "http://127.0.0.1:9000/api/";
 
 export const getPostApiRequest = async () => {
   const response = await axios.get(uri);
